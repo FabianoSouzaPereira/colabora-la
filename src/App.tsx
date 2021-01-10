7f97c4f736267d7018fdaae8960f2562a3a0f516
Fabiano Pereira <jeovajire2003@gmail.com>
2021-01-10 15:08:25 -0300

    Update on air

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import * as React from 'react';
+import * as Updates from 'expo-updates';
 import Routes from './routes';
 import { Provider } from 'react-redux';
 import store from './store';
 
 const App = () => {
+
+  React.useEffect(() => {
+    async function updateApp(){
+      const { isAvailable } = await Updates.checkForUpdateAsync();
+
+      if(isAvailable){
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    }
+
+    updateApp();
+
+  }, []);
+
+
   return (
     <Provider store={store}>
       <Routes />
